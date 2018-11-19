024aba571857d5a710325011585797f1b40961e1
Michał Fladziński <michalfladzinski@outlook.com>
2018-11-20 00:03:38 +0100

    Added chart panel. Added 2d and 3d buttons. Added results label.

--- OptimizationAlpha/OptimizationAlpha/Form1.Designer.cs
+++ OptimizationAlpha/OptimizationAlpha/Form1.Designer.cs
@@ -33,6 +33,10 @@
             this.button_find_min = new System.Windows.Forms.Button();
             this.button_find_max = new System.Windows.Forms.Button();
             this.button_exit = new System.Windows.Forms.Button();
+            this.panel_chart = new System.Windows.Forms.Panel();
+            this.button_2d = new System.Windows.Forms.Button();
+            this.button_3d = new System.Windows.Forms.Button();
+            this.label_results = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_function
@@ -79,11 +83,56 @@
             this.button_exit.UseVisualStyleBackColor = true;
             this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
             // 
+            // panel_chart
+            // 
+            this.panel_chart.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel_chart.Location = new System.Drawing.Point(12, 60);
+            this.panel_chart.Name = "panel_chart";
+            this.panel_chart.Size = new System.Drawing.Size(913, 549);
+            this.panel_chart.TabIndex = 4;
+            // 
+            // button_2d
+            // 
+            this.button_2d.Location = new System.Drawing.Point(12, 38);
+            this.button_2d.Name = "button_2d";
+            this.button_2d.Size = new System.Drawing.Size(75, 23);
+            this.button_2d.TabIndex = 0;
+            this.button_2d.TabStop = false;
+            this.button_2d.Text = "2D";
+            this.button_2d.UseVisualStyleBackColor = true;
+            this.button_2d.Click += new System.EventHandler(this.button_2d_Click);
+            // 
+            // button_3d
+            // 
+            this.button_3d.Location = new System.Drawing.Point(93, 38);
+            this.button_3d.Name = "button_3d";
+            this.button_3d.Size = new System.Drawing.Size(75, 23);
+            this.button_3d.TabIndex = 1;
+            this.button_3d.TabStop = false;
+            this.button_3d.Text = "3D";
+            this.button_3d.UseVisualStyleBackColor = true;
+            this.button_3d.Click += new System.EventHandler(this.button_3d_Click);
+            // 
+            // label_results
+            // 
+            this.label_results.AutoSize = true;
+            this.label_results.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label_results.Location = new System.Drawing.Point(1038, 60);
+            this.label_results.Name = "label_results";
+            this.label_results.Size = new System.Drawing.Size(82, 25);
+            this.label_results.TabIndex = 5;
+            this.label_results.Text = "Results:";
+            this.label_results.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1263, 621);
+            this.Controls.Add(this.label_results);
+            this.Controls.Add(this.button_3d);
+            this.Controls.Add(this.panel_chart);
+            this.Controls.Add(this.button_2d);
             this.Controls.Add(this.button_exit);
             this.Controls.Add(this.button_find_max);
             this.Controls.Add(this.button_find_min);
@@ -105,6 +154,10 @@
         private System.Windows.Forms.Button button_find_min;
         private System.Windows.Forms.Button button_find_max;
         private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Panel panel_chart;
+        private System.Windows.Forms.Button button_3d;
+        private System.Windows.Forms.Button button_2d;
+        private System.Windows.Forms.Label label_results;
     }
 }
 
--- OptimizationAlpha/OptimizationAlpha/Form1.cs
+++ OptimizationAlpha/OptimizationAlpha/Form1.cs
@@ -21,12 +21,12 @@ namespace OptimizationAlpha
 
         private void button_find_min_Click(object sender, EventArgs e)
         {
-            //ODPALA ALGORYTMY SZUKAJĄCE MINIMUM FUNKCJI
+            //ODPALA ALGORYTMY SZUKAJĄCE MINIMUM FUNKCJI ORAZ RYSUJE WYKRESY
         }
 
         private void button_find_max_Click(object sender, EventArgs e)
         {
-            //ODPALA ALGORYTMY SZUKAJĄCE MAXIMUM FUNKCJI
+            //ODPALA ALGORYTMY SZUKAJĄCE MAXIMUM FUNKCJI ORAZ RYSUJE WYKRESY
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -43,27 +43,56 @@ namespace OptimizationAlpha
             button_find_max.Location = new Point(button_find_min.Location.X + button_find_min.Width + Convert.ToInt32(this.Width * 0.025), Convert.ToInt32(this.Height * 0.05));
             button_find_max.Width = Convert.ToInt32(this.Width * 0.15);
 
+            //Location of 2D button
+            button_2d.Location = new Point(Convert.ToInt32(this.Width * 0.05), textBox_function.Location.Y + textBox_function.Height + Convert.ToInt32(this.Height * 0.01));
+            button_2d.Width = Convert.ToInt32(this.Width * 0.075);
+
+            //Location of 3D button
+            button_3d.Location = new Point(button_2d.Location.X + button_2d.Width, button_2d.Location.Y);
+            button_3d.Width = Convert.ToInt32(this.Width * 0.075);
+
+            //Location of 2D chart
+            panel_chart.Location = new Point(Convert.ToInt32(this.Width * 0.05), button_2d.Location.Y + button_2d.Height);
+            panel_chart.Width = Convert.ToInt32(this.Width * 0.725);
+            panel_chart.Height = Convert.ToInt32((this.Height - Convert.ToInt32(this.Height * 0.05) - Convert.ToInt32(this.Height * 0.01) - textBox_function.Height - button_2d.Height) * 0.885);
+
+            //Location of results label
+            label_results.Location = new Point(button_find_max.Location.X, button_find_max.Location.Y + Convert.ToInt32(this.Height * 0.1));
+
             //Location of Button exit
-            button_exit.Location = new Point(button_find_max.Location.X, Convert.ToInt32(this.Height * 0.85));
+            button_exit.Location = new Point(button_find_max.Location.X, Convert.ToInt32(this.Height - button_exit.Height - this.Height * 0.1));
             button_exit.Width = Convert.ToInt32(this.Width * 0.15);
         }
 
         private void textBox_function_Text_Click(object sender, EventArgs e)
         {
-            textBox_function.Text = "";
+            if (textBox_function.Text == "Write a function...")
+            {
+                textBox_function.Text = "";
+            }
         }
 
         private void textBox_function_Leave(object sender, EventArgs e)
         {
             if (textBox_function.Text == "")
             {
-                textBox_function.Text = "Write a function";
+                textBox_function.Text = "Write a function...";
             }
         }
 
         private void button_exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button_2d_Click(object sender, EventArgs e)
+        {
+            //WYKRES 2D na panel_chart
+        }
+
+        private void button_3d_Click(object sender, EventArgs e)
+        {
+            //WYKRES 3D na panel_chart
         }
     }
 }
