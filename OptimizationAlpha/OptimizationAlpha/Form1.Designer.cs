d5b17cc3684566256813470a35e4e75bef184ee7
Michał Fladziński <michalfladzinski@outlook.com>
2018-11-26 16:12:48 +0100

    New layout. Everything changed.

--- OptimizationAlpha/OptimizationAlpha/Form1.Designer.cs
+++ OptimizationAlpha/OptimizationAlpha/Form1.Designer.cs
@@ -30,119 +30,202 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox_function = new System.Windows.Forms.TextBox();
-            this.button_find_min = new System.Windows.Forms.Button();
-            this.button_find_max = new System.Windows.Forms.Button();
-            this.button_exit = new System.Windows.Forms.Button();
-            this.panel_chart = new System.Windows.Forms.Panel();
-            this.button_2d = new System.Windows.Forms.Button();
-            this.button_3d = new System.Windows.Forms.Button();
             this.label_results = new System.Windows.Forms.Label();
+            this.tabControl_chart = new System.Windows.Forms.TabControl();
+            this.tabPage_2d = new System.Windows.Forms.TabPage();
+            this.tabPage_3d = new System.Windows.Forms.TabPage();
+            this.groupBox_functions = new System.Windows.Forms.GroupBox();
+            this.radioButton_two_var = new System.Windows.Forms.RadioButton();
+            this.listBox_variables = new System.Windows.Forms.ListBox();
+            this.radioButton_one_var = new System.Windows.Forms.RadioButton();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_help = new System.Windows.Forms.Button();
+            this.button_search = new System.Windows.Forms.Button();
+            this.button_read_from_file = new System.Windows.Forms.Button();
+            this.tabControl_chart.SuspendLayout();
+            this.groupBox_functions.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_function
             // 
-            this.textBox_function.Location = new System.Drawing.Point(12, 12);
+            this.textBox_function.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_function.Enabled = false;
+            this.textBox_function.Location = new System.Drawing.Point(869, 90);
             this.textBox_function.Name = "textBox_function";
-            this.textBox_function.Size = new System.Drawing.Size(913, 20);
+            this.textBox_function.Size = new System.Drawing.Size(301, 20);
             this.textBox_function.TabIndex = 0;
             this.textBox_function.TabStop = false;
             this.textBox_function.Text = "Write a function...";
             this.textBox_function.Click += new System.EventHandler(this.textBox_function_Text_Click);
             this.textBox_function.Leave += new System.EventHandler(this.textBox_function_Leave);
             // 
-            // button_find_min
-            // 
-            this.button_find_min.Location = new System.Drawing.Point(931, 12);
-            this.button_find_min.Name = "button_find_min";
-            this.button_find_min.Size = new System.Drawing.Size(159, 23);
-            this.button_find_min.TabIndex = 1;
-            this.button_find_min.TabStop = false;
-            this.button_find_min.Text = "Find min";
-            this.button_find_min.UseVisualStyleBackColor = true;
-            this.button_find_min.Click += new System.EventHandler(this.button_find_min_Click);
-            // 
-            // button_find_max
-            // 
-            this.button_find_max.Location = new System.Drawing.Point(1096, 12);
-            this.button_find_max.Name = "button_find_max";
-            this.button_find_max.Size = new System.Drawing.Size(159, 23);
-            this.button_find_max.TabIndex = 2;
-            this.button_find_max.TabStop = false;
-            this.button_find_max.Text = "Find max";
-            this.button_find_max.UseVisualStyleBackColor = true;
-            this.button_find_max.Click += new System.EventHandler(this.button_find_max_Click);
-            // 
-            // button_exit
-            // 
-            this.button_exit.Location = new System.Drawing.Point(1096, 586);
-            this.button_exit.Name = "button_exit";
-            this.button_exit.Size = new System.Drawing.Size(159, 23);
-            this.button_exit.TabIndex = 3;
-            this.button_exit.TabStop = false;
-            this.button_exit.Text = "Exit";
-            this.button_exit.UseVisualStyleBackColor = true;
-            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
-            // 
-            // panel_chart
-            // 
-            this.panel_chart.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel_chart.Location = new System.Drawing.Point(12, 60);
-            this.panel_chart.Name = "panel_chart";
-            this.panel_chart.Size = new System.Drawing.Size(913, 549);
-            this.panel_chart.TabIndex = 4;
-            // 
-            // button_2d
-            // 
-            this.button_2d.Location = new System.Drawing.Point(12, 38);
-            this.button_2d.Name = "button_2d";
-            this.button_2d.Size = new System.Drawing.Size(75, 23);
-            this.button_2d.TabIndex = 0;
-            this.button_2d.TabStop = false;
-            this.button_2d.Text = "2D";
-            this.button_2d.UseVisualStyleBackColor = true;
-            this.button_2d.Click += new System.EventHandler(this.button_2d_Click);
-            // 
-            // button_3d
-            // 
-            this.button_3d.Location = new System.Drawing.Point(93, 38);
-            this.button_3d.Name = "button_3d";
-            this.button_3d.Size = new System.Drawing.Size(75, 23);
-            this.button_3d.TabIndex = 1;
-            this.button_3d.TabStop = false;
-            this.button_3d.Text = "3D";
-            this.button_3d.UseVisualStyleBackColor = true;
-            this.button_3d.Click += new System.EventHandler(this.button_3d_Click);
-            // 
             // label_results
             // 
+            this.label_results.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label_results.AutoSize = true;
             this.label_results.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.label_results.Location = new System.Drawing.Point(1038, 60);
+            this.label_results.Location = new System.Drawing.Point(869, 142);
             this.label_results.Name = "label_results";
             this.label_results.Size = new System.Drawing.Size(82, 25);
             this.label_results.TabIndex = 5;
             this.label_results.Text = "Results:";
             this.label_results.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // tabControl_chart
+            // 
+            this.tabControl_chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl_chart.Controls.Add(this.tabPage_2d);
+            this.tabControl_chart.Controls.Add(this.tabPage_3d);
+            this.tabControl_chart.Location = new System.Drawing.Point(0, 0);
+            this.tabControl_chart.Name = "tabControl_chart";
+            this.tabControl_chart.SelectedIndex = 0;
+            this.tabControl_chart.Size = new System.Drawing.Size(863, 621);
+            this.tabControl_chart.TabIndex = 6;
+            // 
+            // tabPage_2d
+            // 
+            this.tabPage_2d.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_2d.Name = "tabPage_2d";
+            this.tabPage_2d.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_2d.Size = new System.Drawing.Size(855, 595);
+            this.tabPage_2d.TabIndex = 0;
+            this.tabPage_2d.Text = "2D";
+            this.tabPage_2d.UseVisualStyleBackColor = true;
+            this.tabPage_2d.Click += new System.EventHandler(this.tabPage_2d_Click);
+            // 
+            // tabPage_3d
+            // 
+            this.tabPage_3d.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_3d.Name = "tabPage_3d";
+            this.tabPage_3d.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_3d.Size = new System.Drawing.Size(855, 595);
+            this.tabPage_3d.TabIndex = 1;
+            this.tabPage_3d.Text = "3D";
+            this.tabPage_3d.UseVisualStyleBackColor = true;
+            this.tabPage_3d.Click += new System.EventHandler(this.tabPage_3d_Click);
+            // 
+            // groupBox_functions
+            // 
+            this.groupBox_functions.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox_functions.Controls.Add(this.radioButton_two_var);
+            this.groupBox_functions.Controls.Add(this.button_help);
+            this.groupBox_functions.Controls.Add(this.listBox_variables);
+            this.groupBox_functions.Controls.Add(this.radioButton_one_var);
+            this.groupBox_functions.Location = new System.Drawing.Point(869, 12);
+            this.groupBox_functions.Name = "groupBox_functions";
+            this.groupBox_functions.Size = new System.Drawing.Size(382, 72);
+            this.groupBox_functions.TabIndex = 0;
+            this.groupBox_functions.TabStop = false;
+            this.groupBox_functions.Text = "Search function";
+            // 
+            // radioButton_two_var
+            // 
+            this.radioButton_two_var.AutoSize = true;
+            this.radioButton_two_var.Location = new System.Drawing.Point(7, 42);
+            this.radioButton_two_var.Name = "radioButton_two_var";
+            this.radioButton_two_var.Size = new System.Drawing.Size(68, 17);
+            this.radioButton_two_var.TabIndex = 2;
+            this.radioButton_two_var.Text = "maximum";
+            this.radioButton_two_var.UseVisualStyleBackColor = true;
+            this.radioButton_two_var.CheckedChanged += new System.EventHandler(this.radioButton_two_var_CheckedChanged);
+            // 
+            // listBox_variables
+            // 
+            this.listBox_variables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_variables.Enabled = false;
+            this.listBox_variables.FormattingEnabled = true;
+            this.listBox_variables.Items.AddRange(new object[] {
+            "f(x)=",
+            "f(x, y)=",
+            "f(x, y, z)=",
+            "f(x, y, z, ...)="});
+            this.listBox_variables.Location = new System.Drawing.Point(136, 10);
+            this.listBox_variables.Name = "listBox_variables";
+            this.listBox_variables.Size = new System.Drawing.Size(165, 56);
+            this.listBox_variables.TabIndex = 3;
+            this.listBox_variables.SelectedIndexChanged += new System.EventHandler(this.listBox_variables_SelectedIndexChanged);
+            // 
+            // radioButton_one_var
+            // 
+            this.radioButton_one_var.AutoSize = true;
+            this.radioButton_one_var.Location = new System.Drawing.Point(7, 19);
+            this.radioButton_one_var.Name = "radioButton_one_var";
+            this.radioButton_one_var.Size = new System.Drawing.Size(65, 17);
+            this.radioButton_one_var.TabIndex = 0;
+            this.radioButton_one_var.Text = "minimum";
+            this.radioButton_one_var.UseVisualStyleBackColor = true;
+            this.radioButton_one_var.CheckedChanged += new System.EventHandler(this.radioButton_one_var_CheckedChanged);
+            // 
+            // button_exit
+            // 
+            this.button_exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_exit.Location = new System.Drawing.Point(1176, 586);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(71, 23);
+            this.button_exit.TabIndex = 7;
+            this.button_exit.TabStop = false;
+            this.button_exit.Text = "Exit";
+            this.button_exit.UseVisualStyleBackColor = true;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // button_help
+            // 
+            this.button_help.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_help.Location = new System.Drawing.Point(307, 10);
+            this.button_help.Name = "button_help";
+            this.button_help.Size = new System.Drawing.Size(71, 56);
+            this.button_help.TabIndex = 8;
+            this.button_help.TabStop = false;
+            this.button_help.Text = "Help";
+            this.button_help.UseVisualStyleBackColor = true;
+            this.button_help.Click += new System.EventHandler(this.button_help_Click);
+            // 
+            // button_search
+            // 
+            this.button_search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_search.Enabled = false;
+            this.button_search.Location = new System.Drawing.Point(1176, 90);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(71, 20);
+            this.button_search.TabIndex = 9;
+            this.button_search.Text = "Search";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // button_read_from_file
+            // 
+            this.button_read_from_file.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_read_from_file.Enabled = false;
+            this.button_read_from_file.Location = new System.Drawing.Point(869, 116);
+            this.button_read_from_file.Name = "button_read_from_file";
+            this.button_read_from_file.Size = new System.Drawing.Size(378, 23);
+            this.button_read_from_file.TabIndex = 10;
+            this.button_read_from_file.Text = "Read from file";
+            this.button_read_from_file.UseVisualStyleBackColor = true;
+            this.button_read_from_file.Click += new System.EventHandler(this.button_read_from_file_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1263, 621);
-            this.Controls.Add(this.label_results);
-            this.Controls.Add(this.button_3d);
-            this.Controls.Add(this.panel_chart);
-            this.Controls.Add(this.button_2d);
+            this.Controls.Add(this.button_read_from_file);
+            this.Controls.Add(this.button_search);
             this.Controls.Add(this.button_exit);
-            this.Controls.Add(this.button_find_max);
-            this.Controls.Add(this.button_find_min);
+            this.Controls.Add(this.groupBox_functions);
+            this.Controls.Add(this.tabControl_chart);
+            this.Controls.Add(this.label_results);
             this.Controls.Add(this.textBox_function);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(480, 480);
+            this.MinimumSize = new System.Drawing.Size(800, 480);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Extrema System Search";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tabControl_chart.ResumeLayout(false);
+            this.groupBox_functions.ResumeLayout(false);
+            this.groupBox_functions.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,13 +234,18 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_function;
-        private System.Windows.Forms.Button button_find_min;
-        private System.Windows.Forms.Button button_find_max;
-        private System.Windows.Forms.Button button_exit;
-        private System.Windows.Forms.Panel panel_chart;
-        private System.Windows.Forms.Button button_3d;
-        private System.Windows.Forms.Button button_2d;
         private System.Windows.Forms.Label label_results;
+        private System.Windows.Forms.TabControl tabControl_chart;
+        private System.Windows.Forms.TabPage tabPage_2d;
+        private System.Windows.Forms.TabPage tabPage_3d;
+        private System.Windows.Forms.GroupBox groupBox_functions;
+        private System.Windows.Forms.RadioButton radioButton_two_var;
+        private System.Windows.Forms.RadioButton radioButton_one_var;
+        private System.Windows.Forms.ListBox listBox_variables;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_help;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.Button button_read_from_file;
     }
 }
 
--- OptimizationAlpha/OptimizationAlpha/Form1.cs
+++ OptimizationAlpha/OptimizationAlpha/Form1.cs
@@ -18,65 +18,17 @@ namespace OptimizationAlpha
         {
             InitializeComponent();
         }
-
-        private void button_find_min_Click(object sender, EventArgs e)
-        {
-            //ODPALA ALGORYTMY SZUKAJĄCE MINIMUM FUNKCJI ORAZ RYSUJE WYKRESY...
-        }
-
-        private void button_find_max_Click(object sender, EventArgs e)
-        {
-            //ODPALA ALGORYTMY SZUKAJĄCE MAXIMUM FUNKCJI ORAZ RYSUJE WYKRESY...
-        }
-
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            //Location of Texbox with function while resize
-            textBox_function.Location = new Point(Convert.ToInt32(this.Width * 0.05), Convert.ToInt32(this.Height * 0.05));
-            textBox_function.Width = Convert.ToInt32(this.Width * 0.55);
-
-            //Location of Button finding minimum of function while resize
-            button_find_min.Location = new Point(textBox_function.Location.X + textBox_function.Width + Convert.ToInt32(this.Width * 0.025), Convert.ToInt32(this.Height * 0.05));
-            button_find_min.Width = Convert.ToInt32(this.Width * 0.15);
-
-            //Location of Button finding maximum of function while resize
-            button_find_max.Location = new Point(button_find_min.Location.X + button_find_min.Width + Convert.ToInt32(this.Width * 0.025), Convert.ToInt32(this.Height * 0.05));
-            button_find_max.Width = Convert.ToInt32(this.Width * 0.15);
-
-            //Location of 2D button
-            button_2d.Location = new Point(Convert.ToInt32(this.Width * 0.05), textBox_function.Location.Y + textBox_function.Height + Convert.ToInt32(this.Height * 0.01));
-            button_2d.Width = Convert.ToInt32(this.Width * 0.075);
-
-            //Location of 3D button
-            button_3d.Location = new Point(button_2d.Location.X + button_2d.Width, button_2d.Location.Y);
-            button_3d.Width = Convert.ToInt32(this.Width * 0.075);
-
-            //Location of 2D chart
-            panel_chart.Location = new Point(Convert.ToInt32(this.Width * 0.05), button_2d.Location.Y + button_2d.Height);
-            panel_chart.Width = Convert.ToInt32(this.Width * 0.725);
-            panel_chart.Height = Convert.ToInt32((this.Height - Convert.ToInt32(this.Height * 0.05) - Convert.ToInt32(this.Height * 0.01) - textBox_function.Height - button_2d.Height) * 0.885);
-
-            //Location of results label
-            label_results.Location = new Point(button_find_max.Location.X, button_find_max.Location.Y + Convert.ToInt32(this.Height * 0.1));
-
-            //Location of Button exit
-            button_exit.Location = new Point(button_find_max.Location.X, Convert.ToInt32(this.Height - button_exit.Height - this.Height * 0.1));
-            button_exit.Width = Convert.ToInt32(this.Width * 0.15);
-        }
-
+        
         private void textBox_function_Text_Click(object sender, EventArgs e)
         {
-            if (textBox_function.Text == "Write a function...")
-            {
                 textBox_function.Text = "";
-            }
         }
 
         private void textBox_function_Leave(object sender, EventArgs e)
         {
             if (textBox_function.Text == "")
             {
-                textBox_function.Text = "Write a function...";
+                textBox_function.Text = listBox_variables.Items[listBox_variables.SelectedIndex].ToString();
             }
         }
 
@@ -85,14 +37,47 @@ namespace OptimizationAlpha
             Application.Exit();
         }
 
-        private void button_2d_Click(object sender, EventArgs e)
+        private void radioButton_one_var_CheckedChanged(object sender, EventArgs e)
         {
-            //WYKRES 2D na panel_chart...
+            listBox_variables.Enabled = true;
         }
 
-        private void button_3d_Click(object sender, EventArgs e)
+        private void radioButton_two_var_CheckedChanged(object sender, EventArgs e)
         {
-            //WYKRES 3D na panel_chart...
+            listBox_variables.Enabled = true;
+        }
+
+        private void listBox_variables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_function.Enabled = true;
+            button_search.Enabled = true;
+            button_read_from_file.Enabled = true;
+            textBox_function.Text = listBox_variables.Items[listBox_variables.SelectedIndex].ToString();
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            // CODE FOR SEARCHING MIN/MAX ALGHORITM
+        }
+
+        private void button_help_Click(object sender, EventArgs e)
+        {
+            // DISPLAY HELP
+        }
+
+        private void tabPage_2d_Click(object sender, EventArgs e)
+        {
+            // DISPLAY 2d CHART
+        }
+
+        private void tabPage_3d_Click(object sender, EventArgs e)
+        {
+            // DISPLAY 3D CHART
+        }
+
+        private void button_read_from_file_Click(object sender, EventArgs e)
+        {
+            // READ FROM FILE
         }
     }
 }
