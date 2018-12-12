f9710c6e97714e3710c269614261aff8728bdfac
KrzysztofB <ka.badkowski@gmail.com>
2018-12-13 00:55:03 +0100

    HelpForm upgraded

--- OptimizationAlpha/OptimizationAlpha/HelpForm/HelpForm.Designer.cs
+++ OptimizationAlpha/OptimizationAlpha/HelpForm/HelpForm.Designer.cs
@@ -29,42 +29,76 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpForm));
-            this.TextBoxWindowHelp = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listOfFunctionsBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // TextBoxWindowHelp
-            // 
-            this.TextBoxWindowHelp.Enabled = false;
-            this.TextBoxWindowHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextBoxWindowHelp.Location = new System.Drawing.Point(31, 53);
-            this.TextBoxWindowHelp.Name = "TextBoxWindowHelp";
-            this.TextBoxWindowHelp.ReadOnly = true;
-            this.TextBoxWindowHelp.Size = new System.Drawing.Size(370, 35);
-            this.TextBoxWindowHelp.TabIndex = 0;
-            this.TextBoxWindowHelp.Text = "Help Window";
-            this.TextBoxWindowHelp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.HideSelection = false;
-            this.textBox1.Location = new System.Drawing.Point(31, 134);
+            this.textBox1.Location = new System.Drawing.Point(30, 28);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(370, 203);
+            this.textBox1.Size = new System.Drawing.Size(370, 127);
             this.textBox1.TabIndex = 1;
             this.textBox1.TabStop = false;
             this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Instructions";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "List of functions";
+            // 
+            // listOfFunctionsBox
+            // 
+            this.listOfFunctionsBox.FormattingEnabled = true;
+            this.listOfFunctionsBox.Items.AddRange(new object[] {
+            "Abs(x) - Returns the absolute value ",
+            "Cos(x) - Returns the cosine of the specified angle",
+            "Exp(x) - Returns e raised to the specified power",
+            "Log(x) - Returns the natural (base e) logarithm of a specified number",
+            "Log(x, y) - Returns the logarithm of a specified number in a specified base",
+            "Log10(x) - Returns the base 10 logarithm of a specified number",
+            "Pow(x, y) - Returns a specified number raised to the specified power",
+            "Sign(x) - Returns an integer that indicates the sign of an signed x",
+            "Sin(x) - Returns the sine of the specified angle",
+            "Sqrt(x) - Returns the square root of a specified number",
+            "Tan(x) - Returns the tangent of the specified angle",
+            "For more functions visit the site: https://bit.ly/2Egsnpq (CLICK ME!)"});
+            this.listOfFunctionsBox.Location = new System.Drawing.Point(30, 177);
+            this.listOfFunctionsBox.Name = "listOfFunctionsBox";
+            this.listOfFunctionsBox.Size = new System.Drawing.Size(370, 160);
+            this.listOfFunctionsBox.TabIndex = 4;
+            this.listOfFunctionsBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(445, 349);
+            this.Controls.Add(this.listOfFunctionsBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.TextBoxWindowHelp);
             this.Name = "HelpForm";
             this.Text = "HelpForm";
             this.ResumeLayout(false);
@@ -73,8 +107,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox TextBoxWindowHelp;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listOfFunctionsBox;
     }
 }
--- OptimizationAlpha/OptimizationAlpha/HelpForm/HelpForm.cs
+++ OptimizationAlpha/OptimizationAlpha/HelpForm/HelpForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,19 @@ namespace OptimizationAlpha
         public HelpForm()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listOfFunctionsBox.SelectedIndex == listOfFunctionsBox.Items.Count - 1)
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want visit site with the documentation?", "Go to link", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    ProcessStartInfo sInfo = new ProcessStartInfo("https://bit.ly/2Egsnpq");
+                    Process.Start(sInfo);
+                }
+            }
         }
     }
 }
